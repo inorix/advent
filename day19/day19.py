fbbe7dbb7f0895367e7ef03693759ccb268e0072
Miklos Blaner <inorix0@gmail.com>
2015-12-20 04:44:59 +0100

    day19: more reasonable parsing of input, better var names in part 1

--- day19/day19.py
+++ day19/day19.py
@@ -1,38 +1,38 @@
 from string import replace
 from random import shuffle
+from re import findall
 
-replacements = [a.strip().split() for a in open('input.txt').readlines()]
+replacements = [a for a in findall(r'(\w+) => (\w+)',open('input.txt').read())]
 
-src = replacements.pop(len(replacements)-1)[0]
-replacements.pop()
+src = open('input.txt').readlines()[-1]
 
 dest = set()
 
-for a in range(len(replacements)):
+for replacement in replacements:
     # j is pointing to the start of the unprocessed part of the string
     i = 0
     j = 0
-    while replacements[a][0] in src[j:]:
+    while replacement[0] in src[j:]:
         # find next place we can replace
-        i = src[j:].index(replacements[a][0])
+        i = src[j:].index(replacement[0])
         # add to the set
-        dest.add(src[:j + i] + replacements[a][2] + src[j + i + len(replacements[a][0]):])
+        dest.add(src[:j + i] + replacement[1] + src[j + i + len(replacement[0]):])
         # update the search start pointer
-        j = j + len(replacements[a][0])
+        j = j + len(replacement[0])
 
 print len(dest)
 
 # sort replacements by length of replacement
-replacements = sorted(replacements, key = lambda x: len(x[2]))[::-1]
+replacements = sorted(replacements, key = lambda x: len(x[1]))[::-1]
 
 a = 0
 replaces = 0
 src_bak = src
 # greedily try the replacements from longest to shortest until we arrive at e (hopefully, or any 1 length result)
 while len(src) > 1:
-    if replacements[a][2] in src:
-        replaces += src.count(replacements[a][2])
-        src = replace(src, replacements[a][2], replacements[a][0])
+    if replacements[a][1] in src:
+        replaces += src.count(replacements[a][1])
+        src = replace(src, replacements[a][1], replacements[a][0])
         a = 0
     else:
         a += 1
