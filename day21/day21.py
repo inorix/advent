fae81c62a77010922cd51aa0fc85d31c7ad3183a
Miklos Blaner <inorix0@gmail.com>
2015-12-21 18:31:57 +0100

    Day 21

---
+++ day21/day21.py
@@ -0,0 +1,33 @@
+import math
+
+boss_hp, boss_damage, boss_armor = 103.0, 9, 2
+# format for items is [cost, damage, armor]
+# must use a weapon so not considering [0,0,0]
+weapons = [8,4,0],[10,5,0],[25,6,0],[40,7,0],[74,8,0]
+rings = [0,0,0],[25,1,0],[50,2,0],[100,3,0],[20,0,1],[40,0,2],[80,0,3]
+armors = [0,0,0],[13,0,1],[31,0,2],[53,0,3],[75,0,4],[102,0,5]
+
+def config():
+    for a in weapons:
+        for b in armors:
+            for c in rings:
+                for d in rings:
+                    # no same ring twice
+                    if c is d:
+                        continue
+                    # yield cost, damage, armor
+                    yield sum(zip(a,b,c,d)[0]), a[1]+c[1]+d[1], b[2]+c[2]+d[2]
+
+part1 = 1000
+part2 = 0
+
+# loop all possible equipments
+for cost, damage, armor in config():
+    boss_dies = math.ceil(boss_hp/max(1, damage - boss_armor))
+    player_dies = math.ceil(100.0/max(1, boss_damage - armor))
+    if player_dies >= boss_dies:
+        part1 = min(part1, cost)
+    else:
+        part2 = max(part2, cost)
+   
+print part1,part2
