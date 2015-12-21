51b1b6980fc5584cb6468799b646bab1a0657428
Miklos Blaner <inorix0@gmail.com>
2015-12-21 23:48:52 +0100

    day18: more pythonic inner for loops and changing some lists to tuples

--- day18/day18.py
+++ day18/day18.py
@@ -18,20 +18,20 @@ def solve(part):
             for x in range(maxlen):
                 s = 0
                 #check neighbors if they are inside the grid
-                for n in [[-1,-1],[0,-1],[1,-1],[-1,0],[1,0],[-1,1],[0,1],[1,1]]:
-                    if x+n[0] >= 0 and x+n[0] < maxlen and y+n[1] >= 0 and y+n[1] < maxlen:
-                        if grid[x+n[0]][y+n[1]] == 1:
+                for xn, yn in (-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1):
+                    if x+xn >= 0 and x+xn < maxlen and y+yn >= 0 and y+yn < maxlen:
+                        if grid[x+xn][y+yn] == 1:
                             s += 1
                 #changes are stored until we finish checking everything
                 if grid[x][y] == 1 and s < 2 or s > 3:
                     #skip corners for part 2
-                    if part == 1 or [x, y] not in [[0, 0], [maxlen-1, 0], [0, maxlen-1], [maxlen-1, maxlen-1]]:
+                    if part == 1 or (x, y) not in ((0, 0), (maxlen-1, 0), (0, maxlen-1), (maxlen-1, maxlen-1)):
                         change.append([x, y, 0])
                 if grid[x][y] == 0 and s == 3:
-                    change.append([x, y, 1])
+                    change.append((x, y, 1))
         #apply changes
-        for c in change:
-            grid[c[0]][c[1]] = c[2]
+        for xn, yn, cn in change:
+            grid[xn][yn] = cn
             
     return sum(sum(grid, []))
 
