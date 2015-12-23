cb117b5fbc0b63cc050f8cb252b56a3e69498432
Miklos Blaner <inorix0@gmail.com>
2015-12-23 07:10:15 +0100

    day23: fixed potential underflowing of PC to a negative index

--- day23/day23.py
+++ day23/day23.py
@@ -3,33 +3,31 @@ lines = []
 for l in open('input.txt').readlines():
     lines.append(l.strip().split())
 
-def assembler(a):
+def vm(a):
     r = {'a': a, 'b': 0}
     pc = 0
     
-    while True:
-        try:
-            line = lines[pc]
-            cmd = line[0]
-            arg1 = line[1].strip(',')
-            if len(line) > 2:
-                arg2 = line[2]
-                
-            if cmd == 'hlf':
-                r[arg1] /= 2
-            elif cmd == 'tpl':
-                r[arg1] *= 3
-            elif cmd == 'inc':
-                r[arg1] += 1
-            # to save a few lines, pc is incremented every time so jumps are tuned 1 instruction back
-            elif cmd == 'jmp':
-                pc = pc + int(arg1) - 1
-            elif cmd == 'jie' and r[arg1] %2 == 0:
-                pc = pc + int(arg2) - 1
-            elif cmd == 'jio' and r[arg1] == 1:
-                pc = pc + int(arg2) - 1
-            pc += 1
-        except IndexError:
-            return r['b']
+    while 0 <= pc < len(lines):
+        line = lines[pc]
+        cmd = line[0]
+        arg1 = line[1].strip(',')
+        if len(line) > 2:
+            arg2 = line[2]
+            
+        if cmd == 'hlf':
+            r[arg1] /= 2
+        elif cmd == 'tpl':
+            r[arg1] *= 3
+        elif cmd == 'inc':
+            r[arg1] += 1
+        # to save a few lines, pc is incremented every time so jumps are tuned 1 instruction back
+        elif cmd == 'jmp':
+            pc = pc + int(arg1) - 1
+        elif cmd == 'jie' and r[arg1] %2 == 0:
+            pc = pc + int(arg2) - 1
+        elif cmd == 'jio' and r[arg1] == 1:
+            pc = pc + int(arg2) - 1
+        pc += 1
+    return r['b']
     
-print assembler(0), assembler(1)
+print vm(0), vm(1)
